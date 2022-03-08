0e3214282bf29500939080829e53a36735895676
Hafizur Rahman Fahim <hafijurrahman046@gmail.com>
2022-03-08 18:37:29 +0600

    added some basic socket event handlers

--- socket-handler/index.js
+++ socket-handler/index.js
@@ -1,6 +1,45 @@
+const { Room } = require("../models");
+
 function SocketHandler(io) {
   return (socket) => {
     socket.emit("welcome");
+    socket.on("init", sendAvailablePUblicRooms({ io, socket }));
+    socket.on("create-room", createRoomHandler({ io, socket }));
+    socket.on("join-room", joinRoomHandler({ io, socket }));
+  };
+}
+
+function sendAvailablePUblicRooms({ socket }) {
+  return async () => {
+    const rooms = await Room.find({ roomType: "public" });
+    socket.emit("available-public-rooms", rooms);
+  };
+}
+
+function joinRoomHandler({ io, socket }) {
+  return async (data) => {
+    const room = await Room.findById(data.roomId);
+    if (!room) return;
+    room.currentlyInside.push(data.user);
+    await room.save();
+    socket.join(data.roomId);
+    io.to(data.roomId).emit("user-joined", data.user);
+  };
+}
+
+function createRoomHandler({ io }) {
+  return async (data) => {
+    const room = new Room({
+      roomType: data.roomType,
+      author: data.user,
+      currentlyInside: [],
+      bookUrl: data.bookUrl,
+    });
+    const dbResponse = await room.save();
+    //socket.join(dbResponse._id);
+    if (data.roomType === "public") {
+      io.emit("room-created", { roomId: dbResponse._id });
+    }
   };
 }
 
